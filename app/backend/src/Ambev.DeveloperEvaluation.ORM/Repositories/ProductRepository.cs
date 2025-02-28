86b51c107250ab24f06001168ad577846d8e90f7
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-02-28 00:38:41 -0300

    feat: add products repository

---
+++ app/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories;
+
+/// <summary>
+/// Implementation of IProductRepository using Entity Framework Core
+/// </summary>
+public class ProductRepository : IProductRepository
+{
+    private readonly DefaultContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of ProductRepository
+    /// </summary>
+    /// <param name="context">The database context</param>
+    public ProductRepository(DefaultContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// Creates a new product in the database
+    /// </summary>
+    /// <param name="product">The product to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The created product</returns>
+    public async Task<Product> CreateAsync(Product product, CancellationToken cancellationToken)
+    {
+        await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+}
