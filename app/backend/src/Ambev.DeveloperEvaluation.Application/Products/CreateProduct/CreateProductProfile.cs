550f71f078af4d08506534e612e27fb834f64209
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-03-01 11:41:56 -0300

    feat: add create product mappers

---
+++ app/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+
+/// <summary>
+/// Profile for mapping between Application and API CreateProduct responses
+/// </summary>
+public class CreateProductProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for CreateProduct feature
+    /// </summary>
+    public CreateProductProfile()
+    {
+        CreateMap<CreateProductCommand, Product>();
+        CreateMap<Product, CreateProductResult>();
+    }
+}
