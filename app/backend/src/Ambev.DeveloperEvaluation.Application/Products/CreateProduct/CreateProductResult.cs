697d5bf27802829c5c5e5b8a505f64f6fe91911c
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-03-01 11:47:15 -0300

    fix: convert the record create product command response to class

--- app/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductResult.cs
+++ app/backend/src/Ambev.DeveloperEvaluation.Application/Products/CreateProduct/CreateProductResult.cs
@@ -4,14 +4,37 @@
 /// Represents the response returned after successfully creating a new product.
 /// </summary>
 /// <remarks>
-/// This response contains the unique identifier of the newly created product,
-/// which can be used for subsequent operations or reference.
+/// This response contains the details of the newly created product.
 /// </remarks>
-public record CreateProductResult(
-    Guid Id, 
-    string Title, 
-    decimal Price, 
-    string Description, 
-    string Category, 
-    string Image
-);
+public class CreateProductResult
+{
+    /// <summary>
+    /// The unique identifier of the created product
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The product's name
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// The product's price
+    /// </summary>
+    public decimal Price { get; set; }
+    
+    /// <summary>
+    /// The product's description
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// The product's category
+    /// </summary>
+    public string Category { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// The product's image
+    /// </summary>
+    public string Image { get; set; } = string.Empty;
+}
