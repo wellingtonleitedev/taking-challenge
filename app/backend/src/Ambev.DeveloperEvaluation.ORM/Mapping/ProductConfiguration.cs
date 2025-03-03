c608e2df000cb1fa0b74c619a92f7ddc1bb070c2
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-03-03 17:17:40 -0300

    feat: create the product database configuration and update migrations

---
+++ app/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(product => product.Id);
+        builder.Property(product => product.Id)
+            .HasColumnType("uuid")
+            .HasDefaultValueSql("gen_random_uuid()");
+
+        builder.HasIndex(product => product.Title).IsUnique();
+        builder.Property(product => product.Title)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(product => product.Price)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)")
+            .HasPrecision(18, 2);
+
+        builder.Property(product => product.Description)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.HasIndex(product => product.Category);
+        builder.Property(product => product.Category)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(product => product.ImageUrl);
+
+        builder.Property(product => product.CreatedAt)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(product => product.UpdatedAt)
+            .ValueGeneratedOnUpdate();
+    }
+}
--- app/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20250303200731_CreateProductsTable.cs
+++ app/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20250303200731_CreateProductsTable.cs
@@ -16,7 +16,7 @@ namespace Ambev.DeveloperEvaluation.ORM.Migrations
                 table: "Users",
                 type: "timestamp with time zone",
                 nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+                defaultValueSql: "CURRENT_TIMESTAMP");
 
             migrationBuilder.AddColumn<DateTime>(
                 name: "UpdatedAt",
@@ -28,19 +28,30 @@ namespace Ambev.DeveloperEvaluation.ORM.Migrations
                 name: "Products",
                 columns: table => new
                 {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Title = table.Column<string>(type: "text", nullable: false),
-                    Price = table.Column<decimal>(type: "numeric", nullable: false),
-                    Description = table.Column<string>(type: "text", nullable: false),
-                    Category = table.Column<string>(type: "text", nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Description = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Category = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     ImageUrl = table.Column<string>(type: "text", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
                     UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Products", x => x.Id);
                 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Category",
+                table: "Products",
+                column: "Category");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Title",
+                table: "Products",
+                column: "Title",
+                unique: true);
         }
 
         /// <inheritdoc />
